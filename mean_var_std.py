e3fe2937436fcec5495a748d8b6746b16b637d71
Replit user <>
2023-04-01 23:55:50 -0700

    initial commit

--- mean_var_std.py
+++ mean_var_std.py
@@ -1,8 +1,26 @@
 import numpy as np
 
+# The function uses Numpy to output the mean, variance, standard deviation, max, min, and sum of the rows, columns, and elements in a 3 x 3 matrix.
+# The input of the function should be a list containing 9 digits. The function should convert the list into a 3 x 3 Numpy array, and then return a dictionary containing the mean, variance, standard deviation, max, min, and sum along both axes and for the flattened matrix.
+# If a list containing less than 9 elements is passed into the function, it should raise a ValueError exception with the message: "List must contain nine numbers." The values in the returned dictionary should be lists and not Numpy arrays.
+
 def calculate(list):
 
+  if len(list) != 9:
+      raise ValueError("List must contain nine numbers.")
 
+   # Convert the list into a 3 x 3 Numpy array
+  arr = np.array(list).reshape(3, 3)
 
+  # Create a dictionary to store the mean, variance, standard deviation, max, min, and sum along both axes and for the flattened matrix
+  calculations = {}
 
-    return calculations
+  # Calculate the mean, variance, standard deviation, max, min, and sum along both axes and for the flattened matrix
+  calculations["mean"] = [np.mean(arr, axis=0).tolist(), np.mean(arr, axis=1).tolist(), np.mean(arr)]
+  calculations["variance"] = [np.var(arr, axis=0).tolist(), np.var(arr, axis=1).tolist(), np.var(arr)]
+  calculations["standard deviation"] = [np.std(arr, axis=0).tolist(), np.std(arr, axis=1).tolist(), np.std(arr)]
+  calculations["max"] = [np.max(arr, axis=0).tolist(), np.max(arr, axis=1).tolist(), np.max(arr)]
+  calculations["min"] = [np.min(arr, axis=0).tolist(), np.min(arr, axis=1).tolist(), np.min(arr)]
+  calculations["sum"] = [np.sum(arr, axis=0).tolist(), np.sum(arr, axis=1).tolist(), np.sum(arr)]
+
+  return calculations
