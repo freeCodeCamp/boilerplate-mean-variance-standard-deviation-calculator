993217285f18bfb1f0da811bfb1018414882fcbe
bhanumad <128313505+bhanumad@users.noreply.github.com>
2023-12-20 00:16:36 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,22 @@
-# This entrypoint file to be used in development. Start by reading README.md
-import mean_var_std
-from unittest import main
 
-print(mean_var_std.calculate([0,1,2,3,4,5,6,7,8]))
+import numpy as np
 
-# Run unit tests automatically
-main(module='test_module', exit=False)
+def calculate(numbers):
+    # Check if the input list contains exactly 9 elements
+    if len(numbers) != 9:
+        raise ValueError("List must contain nine numbers.")
+
+    # Convert the list to a 3x3 Numpy array
+    matrix = np.array(numbers).reshape(3, 3)
+
+    # Calculate mean, variance, standard deviation, max, min, and sum along axes and flattened matrix
+    result = {
+        'mean': [list(matrix.mean(axis=0)), list(matrix.mean(axis=1)), matrix.mean()],
+        'variance': [list(matrix.var(axis=0)), list(matrix.var(axis=1)), matrix.var()],
+        'standard deviation': [list(matrix.std(axis=0)), list(matrix.std(axis=1)), matrix.std()],
+        'max': [list(matrix.max(axis=0)), list(matrix.max(axis=1)), matrix.max()],
+        'min': [list(matrix.min(axis=0)), list(matrix.min(axis=1)), matrix.min()],
+        'sum': [list(matrix.sum(axis=0)), list(matrix.sum(axis=1)), matrix.sum()]
+    }
+
+    return result
